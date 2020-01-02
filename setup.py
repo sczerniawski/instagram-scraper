2d7b9dc116f373eb5f273b4d9288163e9f10bf8b
Ashot Vardanian <ashvardanian@gmail.com>
2020-01-02 17:16:52 -0300

    Updated Setup.py.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 from pathlib import Path
 
 setuptools.setup(
-    name="igramscraper",
-    version="0.3.2",
+    name='igramscraper',
+    version='0.4.0',
     description=('scrapes medias, likes, followers, tags and all metadata'),
-    long_description=Path("README.md").read_text(),
-    long_description_content_type="text/markdown",
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
-    license="MIT",
-    maintainer="realsirjoe, leungwaiban",
-    author='realsirjoe, leungwaiban',
-    url='https://github.com/realsirjoe/instagram-scraper',
+    license='MIT',
+    maintainer='realsirjoe, leungwaiban, ashvardanian',
+    author='realsirjoe, leungwaiban, ashvardanian',
+    url='https://github.com/ashvardanian/instagram-scraper',
     install_requires=[
         'requests>=2.21.0',
         'python-slugify>=3.0.2'
